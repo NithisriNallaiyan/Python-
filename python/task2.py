f51a5d73e0c9e685af64e69c63a0e0dcea209476
NithisriNallaiyan <152481484+NithisriNallaiyan@users.noreply.github.com>
2023-12-01 14:46:08 +0530

    Add files via upload

---
+++ python/task2.py
@@ -0,0 +1,22 @@
+a=10
+b=2
+name="Nithisri"
+num=10
+print(".............")
+print("variable example")
+print("...................")
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print("...................")
+print("Find type Function")
+print(".....................")
+print(type(a))
+print(type(b))
+print("..................")
+print("id() ")
+print(id(a))
+print(id(b))
+print(id(name))
+print(id(num))
