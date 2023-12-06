087198e110d623c88d252dfcfc501c58fe006b67
NithisriNallaiyan <152481484+NithisriNallaiyan@users.noreply.github.com>
2023-12-06 21:14:32 +0530

    Add files via upload

---
+++ python/task4.py
@@ -0,0 +1,22 @@
+a=10
+list=[10,20,30,40,50]
+if (a in list):
+     print("True")
+else:
+     print("False")
+b=20
+if (b in list):
+     print("True")
+else:
+     print("False")
+a=20
+b=20
+if(a is b):
+     print("True")
+else:
+     print("False")
+b=10
+if(a is not b):
+     print("True")
+else:
+     print("False")
